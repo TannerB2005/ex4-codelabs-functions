04e64b6e2465f06d5f0861040f9dd3293481e79d
Tanner B <sonictanpat@gmail.com>
2025-01-20 19:14:55 -0600

    stuck on 4, 6 finished

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
-console.log(Boolean(0)); // false
-console.log(Boolean("")); // false
-console.log(Boolean("Hello")); // true
-console.log(Boolean(42)); // true
-console.log(Boolean(null)); // false
+// 1. Truthy and Falsy check
+// console.log(Boolean(0)); // false
+// console.log(Boolean("")); // false
+// console.log(Boolean("Hello")); // true
+// console.log(Boolean(42)); // true
+// console.log(Boolean(null)); // false
+
+// 2. Age Eligibility
+// const age = 9;
+// if (age >= 18) {
+//     console.log("You are eligible to vote");
+// } else {
+//     console.log("You are not eligible to vote");
+// }
+
+// 3. Calculate final price
+// let price = 150;
+// let discount = 0.20;
+// let taxRate = 0.8;
+// let finalPrice = price - taxRate * price * discount;
+// console.log(finalPrice); // 126
+
+// console.log(`Original Price: ${price}`);
+// console.log(`Discount: ${discount}`);
+// console.log(`Tax: ${taxRate}`);
+// console.log(`Final Price: ${finalPrice}`)
+
+//4. Nested If-Statements for Login
+
+
+
+//5. Driver Age 
+// age = 18
+// if (age >= 18) {
+//     console.log("You can drive");
+//     } else {
+//         console.log("You need a license to drive.");
+//     }
+
+//6. Logical Operators Practice
+// let isHoliday = false;
+// let isWeekend = true;
+
+// if(isWeekend || isHoliday) {
+//     console.log('You can relax today!');
+// } else {
+//     console.log('It's a workday.');
+// }
