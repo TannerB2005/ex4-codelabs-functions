19c5bcc825dc2f3ac96e8f489a4225a41291d04d
Tanner B <sonictanpat@gmail.com>
2025-01-20 18:38:37 -0600

    finished 1st

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log(Boolean(0)); // false
+console.log(Boolean("")); // false
+console.log(Boolean("Hello")); // true
+console.log(Boolean(42)); // true
+console.log(Boolean(null)); // false
