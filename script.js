39472debb4a35222bbff7d014edf26c5602d970d
Tanner B <sonictanpat@gmail.com>
2025-01-23 14:06:19 -0600

    final commit

--- script.js
+++ script.js
@@ -46,3 +46,61 @@
 // } else {
 //     console.log('It's a workday.');
 // }
+
+//7. Function: greeting
+// function greet(name) {
+//     console.log(`Hello. ${name}!`);
+// }
+
+// greet("John");
+
+//8. Function: Square a Number
+// let num = 5
+// let num2 = 7
+// let num3 = 10
+
+// let squared = num ** 2;
+// let squared2 = num2 ** 2;
+// let squared3 = num3 ** 2;
+
+// console.log(squared, squared2, squared3
+// )
+//9. Function with Default Parameters
+// function calculateDiscount(price = 100, discountRate = 0.1) {
+// 	return price - (price * discountRate);
+// }
+
+// console.log(price)
+
+
+//10. Tax and Discount Calculatorlet 
+function calculatePrice(originalPrice, taxRate, discountRate) {
+    //Convert to decimals
+    taxRate = taxRate / 100;
+    discountRate = discountRate / 100;
+
+    //Calculate Discount
+    const discountAmount = originalPrice * discountRate;
+    const priceAfterDiscount = originalPrice - discountAmount;
+
+    //Calc tax
+    const taxAmount = priceAfterDiscount * taxRate;
+    const finalPrice = priceAfterDiscount + taxAmount;
+
+    return finalPrice;
+}
+
+let originalPrice = 100;
+let taxRate = 10;
+let discountRate = 20;
+
+    console.log(`Original Price: ${originalPrice}`);
+    console.log(`Discount: ${discountRate}%`);
+    console.log(`Tax: ${taxRate}%`);
+
+    const finalPrice = calculatePrice(originalPrice, taxRate, discountRate);
+
+    setTimeout(() => {
+        console.log(`Final Price: ${finalPrice}`);
+    }, 500);
+    
